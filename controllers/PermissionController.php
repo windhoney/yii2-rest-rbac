2bb92c0b76ebf4595ba9968f1b7c23332eeab9a0
Windhoney <524415250@qq.com>
2022-06-09 11:09:27 +0800

    Update PermissionController.php

--- controllers/PermissionController.php
+++ controllers/PermissionController.php
@@ -8,7 +8,6 @@ use yii\rbac\Item;
 /**
  * PermissionController implements the CRUD actions for AuthItem model.
  *
- * @author Misbahul D Munir <misbahuldmunir@gmail.com>
  * @since 1.0
  */
 class PermissionController extends ItemController
